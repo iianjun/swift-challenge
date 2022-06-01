e1fd6325a176cceb8e1cd29da39c57e1a6483c0c
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-05-31 22:32:44 -0400

    update challenges

---
+++ Auth/Login/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by 전하성 on 2022/05/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
